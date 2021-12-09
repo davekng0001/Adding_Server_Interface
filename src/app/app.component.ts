c453b582efca4db38d0d3fa9b2d830c262d57f7f
dakin4 <davekng0001@gmail.com>
2021-12-08 22:36:57 -0500

    minor updates to code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+  serverElements = [];
 
   onServerAdded(serverData: {serverName: string, serverContent: string}) {
-     this.serverElements.push({
+     console.log(serverData);
+    this.serverElements.push({
        type: 'server',
        name: serverData.serverName,
        content: serverData.serverContent
      });
-  }
+    }
+     
 
   onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
      this.serverElements.push({
